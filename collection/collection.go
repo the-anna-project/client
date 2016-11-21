a380156922dd0353686631b2b2931f31aed70c5f
Tim Schindler <xh3b4sd@gmail.com>
2016-11-21 22:42:49 +0100

    updated collection service

--- collection/collection.go
+++ collection/collection.go
@@ -1,6 +1,8 @@
 package collection
 
 import (
+	"sync"
+
 	servicespec "github.com/the-anna-project/spec/service"
 )
 
@@ -12,15 +14,49 @@ func New() servicespec.EndpointCollection {
 type collection struct {
 	// Dependencies.
 
-	textService servicespec.EndpointService
+	metricService servicespec.EndpointService
+	textService   servicespec.EndpointService
+
+	// Settings.
+
+	shutdownOnce sync.Once
 }
 
 func (c *collection) Boot() {
+	//go c.Metric().Boot()
 	go c.Text().Boot()
+}
+
+func (c *collection) Metric() servicespec.EndpointService {
+	return c.metricService
+}
+
+func (c *collection) SetMetricService(metricService servicespec.EndpointService) {
+	c.metricService = metricService
 }
 
 func (c *collection) SetTextService(textService servicespec.EndpointService) {
 	c.textService = textService
+}
+
+func (c *collection) Shutdown() {
+	c.shutdownOnce.Do(func() {
+		var wg sync.WaitGroup
+
+		//wg.Add(1)
+		//go func() {
+		//	c.Metric().Shutdown()
+		//	wg.Done()
+		//}()
+
+		wg.Add(1)
+		go func() {
+			c.Text().Shutdown()
+			wg.Done()
+		}()
+
+		wg.Wait()
+	})
 }
 
 func (c *collection) Text() servicespec.EndpointService {
